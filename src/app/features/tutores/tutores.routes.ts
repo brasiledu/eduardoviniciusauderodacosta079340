973e3852dd9b75b219a0b2a9f5e3c69f459a72a8
Eduardo Vinícius <eduardo.viniciusac@gmail.com>
2026-02-04 21:10:17 -0400

    feat(tutores): configurar rotas do módulo Tutores

--- src/app/features/tutores/tutores.routes.ts
+++ src/app/features/tutores/tutores.routes.ts
@@ -4,5 +4,17 @@ export const TUTORES_ROUTES: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/tutor-list/tutor-list.component').then(m => m.TutorListComponent)
+  },
+  {
+    path: 'novo',
+    loadComponent: () => import('./pages/tutor-form/tutor-form.component').then(m => m.TutorFormComponent)
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('./pages/tutor-detail/tutor-detail.component').then(m => m.TutorDetailComponent)
+  },
+  {
+    path: ':id/editar',
+    loadComponent: () => import('./pages/tutor-form/tutor-form.component').then(m => m.TutorFormComponent)
   }
 ];
